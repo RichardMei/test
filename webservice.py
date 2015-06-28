01e866409beabe8e65b5fd9216ef60bcbb8faa0b
U-PAICDOM\MEIWEI636 <MEIWEI636@ZFQSH-L0175.paicdom.local>
2015-06-28 17:44:23 +0800

    webservice sample

---
+++ webservice.py
@@ -0,0 +1,10 @@
+from suds.client import Client
+
+url = 'http://localhost:8080/cstc-web/services/msettleTestFacade?wsdl' 
+client = Client(url)
+count = 0;
+with open('filename','r') as f:
+	for line in f:
+		result = client.service.sendCommitTransJsonInfo(line)
+		count = count + 1
+		print 'success num:%d, response:%s' % (count, result) 
